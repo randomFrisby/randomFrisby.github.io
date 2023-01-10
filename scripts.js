20ab528b045ccafb5cff7467dc5826b904f764c5
Gaurav Singh <theamazinggaurav@gmail.com>
2023-01-10 22:40:47 +0530

    Projects and github-stats section

--- scripts.js
+++ scripts.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
   
     // typing text animation script
     var typed = new Typed(".typing", {
-      strings: ["Backend Developer", "Problem Solver", "Reader", "Football Nerd"],
+      strings: ["Backend Developer", "Reader", "Football Nerd"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
     });
   
     var typed = new Typed(".typing-2", {
-      strings: ["Backend Developer", "Problem Solver", "Reader", "Football Nerd"],
+      strings: ["Backend Developer", "Reader", "Football Nerd"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
@@ -51,8 +51,8 @@ $(document).ready(function () {
     // owl carousel script
     $(".carousel").owlCarousel({
       margin: 20,
-    loop: false,
-      autoplay: false,   //for carousel effect turn it true
+      loop: true,
+      autoplay: true,   //for carousel effect turn it true
       autoplayTimeOut: 2000,
       autoplayHoverPause: true,
       responsive: {
