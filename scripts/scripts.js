70b25610e4828fc560dd2901f09217ca0e10fa01
Gaurav Singh <theamazinggaurav@gmail.com>
2023-01-16 22:37:43 +0530

    skills and contact section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(window).scroll(function () {
       // sticky navbar on scroll script
-      if (this.scrollY > 150) {
+      if (this.scrollY > 20) {
         $(".navbar").addClass("sticky");
       } else {
         $(".navbar").removeClass("sticky");
